f7128087087a7f1db7e9ac301aba4f9e3ea05e7e
Wojciech Migda <wojtek.golf@interia.pl>
2016-12-21 18:25:58 +0100

    cmake -DCMAKE_CXX_COMPILER=c++.sh -G "Eclipse CDT4 - Unix Makefiles"

---
+++ src/MMRF.hpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Wojciech Migda
+ * All rights reserved
+ * Distributed under the terms of the MIT License
+ *******************************************************************************
+ *
+ * Filename: MMRF.hpp
+ *
+ * Description:
+ *      TCO-MMRF
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2016-12-21   wm              Initial version
+ *
+ ******************************************************************************/
+
+
+#ifndef MMRF_HPP_
+#define MMRF_HPP_
+
+
+#endif /* MMRF_HPP_ */
